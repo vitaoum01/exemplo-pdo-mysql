a97b92130429e35d92d32091faa24fd9dfa60ec0
Vitor Rodrigues <vitor.bidi@gmail.com>
2022-12-01 14:07:44 +0000

    Select Genero

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+require_once './vendor/autoload.php';
+use PDO;
+
+$bd = new PDO('mysql:host=localhost;dbname=biblioteca', 'root', '');
+
+$comando = $bd->prepare('SELECT * FROM generos');
+$comando->execute();
+$generos = $comando->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Biblioteca</title>
+    </head>
+    <body>
+        <table>
+            <tr>
+                <th>Id</th>
+                <th>Nome</th>
+            </tr>
+            <?php foreach($generos as $g): ?>
+                <tr>
+                    <td><?= $g['id'] ?></td>
+                    <td><?= $g['nome'] ?></td>
+                </tr>
+            <?php endforeach ?>
+        </table>
+    </body>
